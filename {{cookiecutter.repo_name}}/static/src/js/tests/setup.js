e4a686560f832f606d5c3ec424abee5fe06d8728
Taavi Teska <taaviteska@gmail.com>
2017-08-16 18:06:04 +0300

    Update node and it's packages setup

--- {{cookiecutter.repo_name}}/static/src/js/tests/setup.js
+++ {{cookiecutter.repo_name}}/static/src/js/tests/setup.js
@@ -1,10 +1,10 @@
-import { jsdom } from 'jsdom';
+import { JSDOM } from 'jsdom';
 import setupBrowserGlobals from 'jsdom-global';
 
 
 setupBrowserGlobals();
 
-global.document = jsdom('');
+global.document = new JSDOM('');
 global.window = document.defaultView;
 global.navigator = { userAgent: 'node.js' };
 global.gettext = str => str;
